f832d06e6c663bc769dfe996f9121cabeb5b2326
narminmammadova1 <nerminytb89@gmail.com>
2023-08-06 23:59:43 +0400

    added new js file

--- main.js
+++ main.js
@@ -1,22 +1,4 @@
-const myTitle=document.querySelector("#myTitle")
-const myScore=document.querySelector("#myScore")
-const myImg=document.querySelector("#myImg")
-const myName=document.querySelector("#myName")
 
-const compTitle=document.querySelector("#compTitle")
-const compScore=document.querySelector("#compScore")
-const compImg=document.querySelector("#compImg")
-const  compName=document.querySelector("#compName")
-
-const buttonStart=document.querySelector("#buttonStart")
-const buttonRemote=document.querySelector("#buttonRemote")
-const buttonKlav=document.querySelector("#buttonKlav")
-const buttonRock=document.querySelector("#buttonRock")
-const buttonPaper=document.querySelector("#buttonPaper")
-const buttonScissors=document.querySelector("#buttonScissors")
-
-const leftDiv=document.querySelector("#leftDiv")
-const rightDiv=document.querySelector("#rightDiv")
 let btn=document.getElementsByClassName("btn")
 let hands=["r","p","s"]
 let player1Score=0
---
+++ varible.js
@@ -0,0 +1,19 @@
+const myTitle=document.querySelector("#myTitle")
+const myScore=document.querySelector("#myScore")
+const myImg=document.querySelector("#myImg")
+const myName=document.querySelector("#myName")
+
+const compTitle=document.querySelector("#compTitle")
+const compScore=document.querySelector("#compScore")
+const compImg=document.querySelector("#compImg")
+const  compName=document.querySelector("#compName")
+
+const buttonStart=document.querySelector("#buttonStart")
+const buttonRemote=document.querySelector("#buttonRemote")
+const buttonKlav=document.querySelector("#buttonKlav")
+const buttonRock=document.querySelector("#buttonRock")
+const buttonPaper=document.querySelector("#buttonPaper")
+const buttonScissors=document.querySelector("#buttonScissors")
+
+const leftDiv=document.querySelector("#leftDiv")
+const rightDiv=document.querySelector("#rightDiv")
